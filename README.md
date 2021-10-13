6b1707d05c7564aaebcb2da72d8caa451dcfa959
ARIELOZAM <arielloza71@gmail.com>
2021-10-13 15:28:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-jhszwq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-jhszwq)
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text-center">
+          <h2>Welcome</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
